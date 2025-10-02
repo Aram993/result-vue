e4ef85dcd31b30c4dc9ffa570850791d2407cb2c
Aram993 <aram993@mail.ru>
2025-10-02 20:13:41 +0300

    comp

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <button @click="newNumber++">{{ text }} {{ number }} {{ newNumber }} {{ fixedNumber }}</button>
+</template>
+<script setup>
+import { computed, ref } from 'vue';
+
+  const props = defineProps({
+    text: {
+      type: String,
+      // required: true,
+      // default: "Значение по умолчанию"
+      validator(value) {
+        return ["Добавить", "Удалить"].includes(value)
+      }
+    },
+    number: [Number, String]
+  });
+  const newNumber = ref(props.number);
+  const fixedNumber = computed(()=> props.number.toFixed(5));
+  console.log(props)
+</script>
